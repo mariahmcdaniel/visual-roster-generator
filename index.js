2b7f688b119dddd27d4c649472422490fd914448
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-02 18:26:19 -0500

    edits to prompts, began generate team function

--- index.js
+++ index.js
@@ -25,197 +25,268 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const teamArr = [];
 
-
 prompt([
+  {
+    type: "input",
+    message: "Who is the manager of this team?",
+    name: "mName",
+    validate: (name) => {
+      if (name) {
+        return true;
+      } else {
+        return "Please enter the name of your team's manager to continue!";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: (answers) => `What is ${answers?.mName}'s employee ID?`,
+    name: "mId",
+    validate: (id) => {
+      if (id) {
+        return /^[0-9]*$/.test(id)
+          ? true
+          : "Please enter the manager's employee id - only numerals are accepted";
+      } else {
+        return "Please enter the manager's employee id";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: (answers) => `What is ${answers.mName}'s email address?`,
+    name: "mEmail",
+    validate: function (mEmail) {
+      if (mEmail) {
+        // Regex email check (return true if valid mail) from emailregex.com
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          mEmail
+        )
+          ? true
+          : "Please enter a valid email address!";
+      } else {
+        return "Please enter the manager's email address";
+      }
+    },
+  },
+  {
+    type: "input",
+    message: (answers) => `What is ${answers.mName}'s office number?`,
+    name: "office",
+    validate: (officeNum) => {
+      if (officeNum) {
+        return /^[0-9]*$/.test(officeNum)
+          ? true
+          : "Please enter the office number - only numerals are accepted";
+      } else {
+        return "Please enter the manager's office number";
+      }
+    },
+  },
+  {
+    type: "list",
+    message: "Add team members! Which type of employee would you like to add?",
+    name: "nextEmpRole",
+    choices: ["Engineer", "Intern"],
+  },
+]).then((answers) => {
+  const employee = new Manager(
+    answers.mName,
+    answers.mId,
+    answers.mEmail,
+    answers.office
+  );
+  teamArr.push(employee);
+  console.log(answers);
+  answers.nextEmpRole === "Engineer" ? generateEng() : generateInt();
+});
+
+const generateEng = () => {
+  prompt([
     {
       type: "input",
-          message: "Who is the manager of this team?",
-          name: "mName",
-          validate: (name) => {
-              if (name) {
-                  return true;
-              } else {
-                  return "Please enter the name of your team's manager to continue!" 
-              }
-          },
+      message: "What is this Engineer's name?",
+      name: "eName",
+      validate: (name) => {
+        if (name) {
+          return true;
+        } else {
+          return "Please enter the name of your engineer to continue!";
+        }
       },
-      {
-          type: "input",
-          message:(answers) => `What is ${answers?.mName}'s employee ID?`,
-          name: "mId",
-          validate: (mId) => {
-              return /^[0-9]*$/.test(mId) ? true : "Please enter the employee ID - only numerals are accepted"
-          }
+    },
+    {
+      type: "input",
+      message: (answers) => `What is ${answers?.eName}'s employee ID?`,
+      name: "eId",
+      validate: (id) => {
+        if (id) {
+          return /^[0-9]*$/.test(id)
+            ? true
+            : "Please enter this engineer's employee id - only numerals are accepted";
+        } else {
+          return "Please enter this engineer's employee id";
+        }
       },
-      {
-          type: "input",
-          message:(answers) => `What is ${answers.mName}'s email address?`,
-          name: "mEmail",
-          validate: function(mEmail)
-          {
-              // Regex email check (return true if valid mail) from emailregex.com
-  
-              return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(mEmail) ? true : "Please enter a valid email address!";
-          }
+    },
+    {
+      type: "input",
+      message: (answers) => `What is ${answers.eName}'s email address?`,
+      name: "eEmail",
+      validate: function (email) {
+        if (email) {
+          // Regex email check (return true if valid mail) from emailregex.com
+          return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            email
+          )
+            ? true
+            : "Please enter a valid email address!";
+        } else {
+          return "Please enter the engineer's email address";
+        }
       },
-      {
-          type: "input",
-          message:(answers) => `What is ${answers.mName}'s office number?`,
-          name: "office",
-          validate: (officeNum) => {
-              return /^[0-9]*$/.test(officeNum) ? true : "Please enter the office number - only numerals are accepted"
-          }
+    },
+    {
+      type: "input",
+      message: (answers) => `What is ${answers.eName}'s GitHub username?`,
+      name: "gitHub",
+      validate: (username) => {
+        return username
+          ? true
+          : `Please enter ${answers.eName}'s GitHub username to continue!`;
       },
-      {
-          type: "list",
-          message: "Add team members! Which type of employee would you like to add?",
-          name: "nextEmpRole",
-          choices: ["Engineer", "Intern"]
-      },
-  ]).then ((answers) => {
-      const employeeInfo = {name : answers.mName, id : answers.mId, email : answers.mEmail, office : answers.office };
-      const employee = new Manager  
-      teamArr.push(member);  
-      console.log(answers);
-      answers.nextEmpRole === "Engineer" ? generateEng() : generateInt()
+    },
+    {
+      type: "list",
+      message: "Would you like to add another team member?",
+      name: "nextEmpRoleE",
+      choices: [
+        "Add another engineer",
+        "Add an intern",
+        "The team is complete!",
+      ],
+    },
+  ]).then((answers) => {
+    const employee = new Engineer(
+      answers.eName,
+      answers.eId,
+      answers.eEmail,
+      answers.gitHub
+    );
+    teamArr.push(employee);
+    if (answers.nextEmpRoleE === "The team is complete!") {
+      console.log("Your Team's profile is being created!");
+      console.log(teamArr);
+      //   fs.writeFile(`${answers.eName}.HTML`, html, (err) => {
+      //       if(err)
+      //           throw err;
+      //       console.log("Success! Your HTML page has been generated")
+      //   })
+    } else {
+      return answers.nextEmpRoleE === "Add another engineer"
+        ? generateEng()
+        : generateInt();
+    }
   });
-  
-  
-  const generateEng = () => {
-      prompt([
-          {
-              type: "input",
-                  message: "What is this Engineer's name?",
-                  name: "eName",
-                  validate: (name) => {
-                      if (name) {
-                          return true;
-                      } else {
-                          return "Please enter the name of your engineer to continue!" 
-                      }
-                  },
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What is ${answers?.eName}'s employee ID?`,
-                  name: "eId",
-                  validate: (id) => {
-                      return /^[0-9]*$/.test(id);
-                  }
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What is ${answers.eName}'s email address?`,
-                  name: "eEmail",
-                  validate: function(email)
-                  {
-                      // Regex email check (return true if valid mail) from emailregex.com
-          
-                      return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) ? true : "Please enter a valid email address!";
-                  }
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What is ${answers.eName}'s GitHub username?`,
-                  name: "gitHub",
-                  validate: (username) => {
-                      return username ? true : `Please enter ${answers.eName}'s GitHub username to continue!`
-                  }
-              },
-              {
-                  type: "list",
-                  message: "Would you like to add another team member?",
-                  name: "nextEmpRoleE",
-                  choices: ["Add another engineer", "Add an intern", "The team is complete!"]
-              },
-  
-  
-      ]).then ((answers) => {
-          if (answers.nextEmpRoleE === "The team is complete!"){
-              console.log("Your Team's profile is being created!")
-              fs.writeFile(`${answers.eName}.HTML`, html, (err) => {
-                  if(err)
-                      throw err;
-                  console.log("Success! Your HTML page has been generated")
-              })
-          } else {
-             return answers.nextEmpRoleE === "Add another engineer" ? generateEng() : generateInt()
-          }
-      })
-  
-  }
-  
-  //Code from previous challenge for reference
-  
-  // const writeToFile = function (markdown) {
-  //     fs.writeFile('newREADME.md', markdown, (err) => {
-  //       if (err)
-  //         throw err;
-  //       console.log('Success! Your README has been generated')
-  //     });
-  //   };
-  
-  const generateInt = () => {
-      prompt([
-          {
-              type: "input",
-                  message: "What is this Intern's name?",
-                  name: "iName",
-                  validate: (name) => {
-                      if (name) {
-                          return true;
-                      } else {
-                          return "Please enter the name of your intern to continue!" 
-                      }
-                  },
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What is ${answers?.iName}'s employee ID?`,
-                  name: "iId",
-                  validate: (id) => {
-                      return /^[0-9]*$/.test(id);
-                  }
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What is ${answers.iName}'s email address?`,
-                  name: "iEmail",
-                  validate: function(email)
-                  {
-                      // Regex email check (return true if valid mail) from emailregex.com
-          
-                      return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) ? true : "Please enter a valid email address!";
-                  }
-              },
-              {
-                  type: "input",
-                  message:(answers) => `What school does ${answers.iName} attend?`,
-                  name: "school",
-                  validate: (school) => {
-                      return school ? true : `Please enter the name of ${answers.eName}'s school to continue!`
-                  }
-              },
-              {
-                  type: "list",
-                  message: "Would you like to add another team member?",
-                  name: "nextEmpRoleE",
-                  choices: ["Add another intern", "Add an engineer", "The team is complete!"]
-              },
-  
-  
-      ]).then ((answers) => {
-          if (answers.nextEmpRoleI === "The team is complete!"){
-              console.log("Your Team's profile is being created!")
-              fs.writeFile(`${answers.iName}.HTML`, html, (err) => {
-                  if(err)
-                      throw err;
-                  console.log("Success! Your HTML page has been generated")
-              })
-          } else {
-             return answers.nextEmpRoleI === "Add another intern" ? generateInt() : generateEng()
-          }
-      })
-  }
-    
-module.exports = teamArr;
+};
+
+//Code from previous challenge for reference
+
+// const writeToFile = function (markdown) {
+//     fs.writeFile('newREADME.md', markdown, (err) => {
+//       if (err)
+//         throw err;
+//       console.log('Success! Your README has been generated')
+//     });
+//   };
+
+const generateInt = () => {
+  prompt([
+    {
+      type: "input",
+      message: "What is this Intern's name?",
+      name: "iName",
+      validate: (name) => {
+        if (name) {
+          return true;
+        } else {
+          return "Please enter the name of your intern to continue!";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: (answers) => `What is ${answers?.iName}'s employee ID?`,
+      name: "iId",
+      validate: (id) => {
+        if (id) {
+          return /^[0-9]*$/.test(id)
+            ? true
+            : "Please enter this intern's employee id - only numerals are accepted";
+        } else {
+          return "Please enter the intern's employee id";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: (answers) => `What is ${answers.iName}'s email address?`,
+      name: "iEmail",
+      validate: function (email) {
+        if (email) {
+          // Regex email check (return true if valid mail) from emailregex.com
+          return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            email
+          )
+            ? true
+            : "Please enter a valid email address!";
+        } else {
+          return "Please enter the manager's email address";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: (answers) => `What school does ${answers.iName} attend?`,
+      name: "school",
+      validate: (school) => {
+        return school
+          ? true
+          : `Please enter the name of ${answers.eName}'s school to continue!`;
+      },
+    },
+    {
+      type: "list",
+      message: "Would you like to add another team member?",
+      name: "nextEmpRoleI",
+      choices: [
+        "Add another intern",
+        "Add an engineer",
+        "The team is complete!",
+      ],
+    },
+  ]).then((answers) => {
+    const employee = new Intern(
+      answers.iName,
+      answers.iId,
+      answers.iEmail,
+      answers.school
+    );
+    teamArr.push(employee);
+    if (answers.nextEmpRoleI === "The team is complete!") {
+      console.log("Your Team's profile is being created!");
+      console.log(teamArr);
+      //   fs.writeFile(`${answers.iName}.HTML`, html, (err) => {
+      //       if(err)
+      //           throw err;
+      //       console.log("Success! Your HTML page has been generated")
+      //   })
+    } else {
+      return answers.nextEmpRoleI === "Add another intern"
+        ? generateInt()
+        : generateEng();
+    }
+  });
+};
+
+exports.teamArr = teamArr;
--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,50 @@
 // create the team
 const generateTeamMembers = (team) => {
+  const generateManager = (manager) => {
+    return `
+        <article>
+      <h2>Manager</h2>
+      <h3>${manager.getName()}</h3>
+      <ul>
+        <li>ID: ${manager.getId()}</li>
+        <li>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+        <li>Office Number: ${manager.getOfficeNumber()}</li>
+      </ul>
+    </article>
+    
+    `;
+  };
+  const generateEngineer = (engineer) => {
+    return `
+        <article>
+        <h2>Engineer</h2>
+        <h3>${engineer.getName()}</h3>
+        <ul>
+          <li>ID: ${engineer.getId()}</li>
+          <li>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+          <li>GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+        </ul>
+      </article>
+      
+      `;
+  };
+  const generateIntern = (intern) => {
+    return `
+    <article>
+      <h2>Intern</h2>
+      <h3>${intern.getName()}</h3>
+      <ul>
+        <li>ID: ${intern.getId()}</li>
+        <li>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li>School: ${intern.getSchool()}</li>
+      </ul>
+    </article>
 
+    `;
+  };
 };
 
 // export function to generate entire page
 module.exports = (team) => {
-    // TODO: MAIN HTML TEMPLATE LITERAL GOES HERE
+  // TODO: MAIN HTML TEMPLATE LITERAL GOES HERE
 };
