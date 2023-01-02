7a511be29c300d598376cd6c8b021a6a3bdf2c84
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-02 18:39:41 -0500

    generateTeam set up

--- src/page-template.js
+++ src/page-template.js
@@ -42,9 +42,75 @@ const generateTeamMembers = (team) => {
 
     `;
   };
+  const html = [];
+  html.push(
+    team
+      .filter((member) => member.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((member) => member.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+  );
+  html.push(
+    team
+      .filter((member) => member.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+  );
+  return html.join("");
 };
 
 // export function to generate entire page
 module.exports = (team) => {
-  // TODO: MAIN HTML TEMPLATE LITERAL GOES HERE
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Visual Roster</title>
+  <style>
+    @import "https://cdn.simplecss.org/simple.min.css";
+
+    main {
+      display: grid;
+      grid-column: 1/-1;
+      justify-items: center;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 1rem;
+      max-width: 1140px;
+      margin: auto;
+    }
+
+    @media screen and (max-width: 1140px) {
+      main {
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+
+    @media screen and (max-width: 720px) {
+      main {
+        grid-template-columns: 1fr;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <header>
+    <h1>My Team</h1>
+  </header>
+  <main>
+  ${generateTeamMembers(team)}
+  </main>
+  <footer>
+    &copy; 2022-2023
+  </footer>
+</body>
+
+</html>
+  `;
 };
